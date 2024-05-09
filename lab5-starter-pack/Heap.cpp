8333070cfe85121c044df6a754596c7fc93c86a6
TomeT187 <tomet187@gmail.com>
2024-05-09 16:13:59 -0700

    ????

--- lab5-starter-pack/Heap.cpp
+++ lab5-starter-pack/Heap.cpp
@@ -9,13 +9,13 @@ Heap::Heap()
 }
 
 Heap::~Heap() {
-  delete[] arr;
+  delete[] *arr;
 }
 //Other rule of three stuff too!
 
 
 void Heap::enqueue(PrintJob* job) {
-  if(numItems == MAX_HEAP_SIZE) throw runtime_error("heap is full");
+  if(numItems  == MAX_HEAP_SIZE) throw runtime_error("heap is full");
   arr[numItems] = job;
   MaxHeapPercolateUp(numItems);
   numItems++;
@@ -35,9 +35,10 @@ PrintJob* Heap::highest() {
 }
 
 void Heap::print() const {
+  if(numItems == 0) throw runtime_error("heap is empty");
   //Priority: priority, Job Number: jobNum, Number of Pages: numPages
   cout << "Priority: " << arr[0]->getPriority() << ", Job Number: " 
-          << arr[0]->getJobNumber() << ", Number of Pages:" << arr[0]->getPages() << endl;
+          << arr[0]->getJobNumber() << ", Number of Pages: " << arr[0]->getPages() << endl;
 }
 
 
@@ -60,5 +61,35 @@ void Heap::MaxHeapPercolateUp(int nodeIndex) {
 }
 
 void Heap::trickleDown(int nodeIndex){
-  
+  int childIndex = 2 * nodeIndex + 1;
+  int value = arr[nodeIndex]->getPriority();
+
+  int maxValue;
+  int maxIndex;
+
+  PrintJob* swapHolder;
+
+  while (childIndex < numItems) {
+      // Find the max among the node and all the node's children
+      maxValue = value;
+      maxIndex = -1;
+      for (int i = 0; i < 2 && i + childIndex < numItems; i++) {
+         if (arr[i + childIndex]->getPriority() > maxValue) {
+            maxValue = arr[i + childIndex]->getPriority();
+            maxIndex = i + childIndex;
+         }
+      }
+
+      if (maxValue == value) {
+         return;
+      }
+      else {
+        swapHolder = arr[nodeIndex];
+        arr[nodeIndex] = arr[maxIndex];
+        arr[maxIndex] = swapHolder;
+        nodeIndex = maxIndex;
+        childIndex = 2 * nodeIndex + 1;
+      }
+   }
+
 }
